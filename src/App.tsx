08c36e521f613710f3633f626b1a600035862ef4
axell-brendow <breno.axel@gmail.com>
2020-07-28 14:49:34 -0300

    Upload initial files

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { GlobalStyles } from "./styles";
+
+const App = () => (
+  <>
+    <GlobalStyles />
+    <div className="App">Hello :D</div>
+  </>
+);
+
+export default App;
---
+++ src/styles.ts
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    font-family: 'Roboto', sans-serif;
+    margin: 0;
+    padding: 0;
+    outline: 0;
+  }
+
+  :root {
+    --primary: #2A7AE4;
+    --black: #000000;
+    --blackLighter: #9E9E9E;
+    --grayLight: #F5F5F5;
+    --grayMedium: #e5e5e5; 
+    --white: #FFFFFF;
+    --frontEnd: #6BD1FF;
+    --backEnd: #00C86F;
+  }
+
+  a {
+    color: inherit;
+  }
+`;
