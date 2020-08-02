332c005e5aceefd32f149a3edfe251f896b9a2a7
axell-brendow <breno.axel@gmail.com>
2020-08-02 10:00:00 -0300

    Add translations and improve flattenMessages

--- src/utils/flattenMessages.ts
+++ src/utils/flattenMessages.ts
@@ -1,5 +1,7 @@
-interface NestedMessages extends Record<string, string | NestedMessages> {}
 type FlattenMessages = Record<string, string>;
+
+interface NestedMessages
+  extends Record<string, string | NestedMessages | NestedMessages[]> {}
 
 export default function flattenMessages(
   nestedMessages: NestedMessages,
@@ -9,11 +11,20 @@ export default function flattenMessages(
     (messages, key) => {
       const value = nestedMessages[key];
       const prefixedKey = prefix ? `${prefix}.${key}` : key;
+
       if (typeof value === "string") {
         messages[prefixedKey] = value;
+      } else if (Array.isArray(value)) {
+        value.forEach((message, i) =>
+          Object.assign(
+            messages,
+            flattenMessages(message, `${prefixedKey}${i}`)
+          )
+        );
       } else {
         Object.assign(messages, flattenMessages(value, prefixedKey));
       }
+
       return messages;
     },
     {}
