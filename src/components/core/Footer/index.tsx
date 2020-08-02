1efc3388f16586b75c6fa0f84ca2c985da3dcd1d
axell-brendow <breno.axel@gmail.com>
2020-08-02 09:59:05 -0300

    Add Footer

---
+++ src/components/core/Footer/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import * as S from "./styles";
+
+const Footer = () => {
+  return (
+    <S.Footer>
+      <a href="https://www.alura.com.br/">
+        <img
+          src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg"
+          alt="Logo Alura"
+        />
+      </a>
+      <p>
+        Orgulhosamente criado durante a{" "}
+        <a href="https://www.alura.com.br/">Imersão React da Alura</a>
+      </p>
+    </S.Footer>
+  );
+};
+
+export default Footer;
